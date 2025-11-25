84d8171aeeabbeeddb306cabde9882dfd6795361
rareorigin <product@commerceroad.finance>
2025-11-25 22:43:53 +0545

    AST: Configure css module files and imports, TODO - to replace scss @import with @use directives

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,10 @@
 import type { StorybookConfig } from "@storybook/nextjs-vite";
+import {mergeConfig} from "vite";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const dirname =
+  typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
 const config: StorybookConfig = {
   "stories": [
@@ -18,6 +24,22 @@ const config: StorybookConfig = {
   },
   "staticDirs": [
     "../public"
-  ]
+  ],
+  viteFinal: async (config) => {
+    return mergeConfig(config, {
+      resolve: {
+        alias: {
+          "@style": path.resolve(dirname, "../app"),
+        }
+      },
+      css: {
+        preprocessorOptions: {
+          scss: {
+            includePaths: [path.resolve(dirname, "../app")]
+          }
+        }
+      }
+    });
+  },
 };
 export default config;
--- src/stories/Input.tsx
+++ src/stories/Input.tsx
@@ -2,15 +2,11 @@ import * as React from 'react';
 import { Input as BaseInput } from '@/components/ui/input';
 import { cn } from '@/lib/utils';
 
+import styles from './Input.module.scss';
+
 export const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
   ({ className, ...props }, ref) => {
-    return (
-      <BaseInput
-        ref={ref}
-        className={cn('h-12 bg-pr-blue-800 border-pr-blue-600', className)}
-        {...props}
-      />
-    );
+    return <BaseInput ref={ref} className={cn('h-12', styles.input, className)} {...props} />;
   },
 );
 
