4c6e06ad13c15b7b0b080b3ff788a1039a99e17e
rareorigin <product@commerceroad.finance>
2025-12-06 11:30:26 +0545

    AST-master: Fix text colors

--- src/presentation/components/Separator.tsx
+++ src/presentation/components/Separator.tsx
@@ -5,13 +5,13 @@ interface SeparatorProps {
 export const Separator = ({ text }: SeparatorProps) => {
   return (
     <div className="h-4 flex flex-row gap-x-3 items-center justify-center">
-      <div className="h-[1px]  w-full bg-gray-200"></div>
+      <div className="h-[1px]  w-full bg-gray-400"></div>
       {text && (
         <>
-          <div className="text-xs text-gray-200 whitespace-nowrap text-center flex-shrink-0">
+          <div className="text-xs text-gray-400 whitespace-nowrap text-center flex-shrink-0">
             {text}
           </div>
-          <div className="h-[1px] bg-gray-200 w-full"></div>
+          <div className="h-[1px] bg-gray-400 w-full"></div>
         </>
       )}
     </div>
